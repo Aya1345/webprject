e866771dbb9629bcdeeab9703a6960611e77654b
Aya1345 <85703464+Aya1345@users.noreply.github.com>
2022-05-27 05:11:50 +0200

    Add files via upload

---
+++ javascript/coding.js
@@ -0,0 +1,25 @@
+function drop() {
+    const togglebutton = document.getElementsByClassName('toggle-button')[0]
+    const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+    togglebutton.addEventListener('click', () => { navbarlinks.classList.toggle('show') })
+}
+function opfunction(){
+    window.open("https://www.allrecipes.com/recipes/250/main-dish/pizza/","_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=37,left=37,width=1340,height=640")
+  }
+      function opfunction1(){
+    window.open("https://sandersonfarms.com/recipes/sticky-orange-glazed-chicken-wings/","_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=37,left=37,width=1340,height=640")
+  }
+  function opfunction2(){
+    window.open("https://www.allrecipes.com/gallery/strawberry-rhubarb-recipes/","_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=37,left=37,width=1340,height=640")
+  }
+  function opfunction3(){
+    window.open("https://www.allrecipes.com/gallery/memorial-day-cakes/","_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=37,left=37,width=1340,height=640")
+  }
+  
+  function opfunction4(){
+    window.open("https://www.allrecipes.com/recipe/254449/grilled-grapefruit-paloma-cocktail/","_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=37,left=37,width=1340,height=640")
+  }
+  function opfunction5(){
+    window.open("https://www.allrecipes.com/recipe/235850/starbucks-caramel-frappuccino-copycat-recipe/","_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=37,left=37,width=1340,height=640")
+  }
+  
